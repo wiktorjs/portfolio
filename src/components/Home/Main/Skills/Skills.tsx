696f64585fd7b033063c41f2556adc78da748c9b
wiktorjs <wiktorj.sienkiewicz@gmail.com>
2023-06-19 16:31:53 +0200

    Add observer for skills section

--- src/components/Home/Main/Skills/Skills.tsx
+++ src/components/Home/Main/Skills/Skills.tsx
@@ -1,14 +1,16 @@
-import Background from '@/components/UI/Background/Background';
+import { useInView } from 'react-intersection-observer';
 import classes from './Skills.module.scss';
 import skillsArr from '@/store/skills-images';
 
 export default function Skills() {
+  const { ref, inView, entry } = useInView({ threshold: 1, triggerOnce: true });
+
   return (
-    <section id="skills">
+    <section id="skills" ref={ref}>
       <h2>Main Skillset</h2>
       <ul className={classes.list}>
         {skillsArr.map((skill, i) => (
-          <li key={i}>
+          <li key={i} className={`${classes.item} ${inView ? classes.visible : ''}`} style={{transitionDelay: `${i/5}s`}}>
             <img src={skill.image} alt={`${skill.name} logo`} /> {skill.name}
           </li>
         ))}
